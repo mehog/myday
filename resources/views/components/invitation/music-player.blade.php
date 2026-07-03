e37c1737f02c52b0b7fa750257fe2347226a7ce8
Meho Garanovic <meho_g@live.com>
2026-07-03 13:46:43 +0200

    fix yt player

--- resources/views/components/invitation/music-player.blade.php
+++ resources/views/components/invitation/music-player.blade.php
@@ -7,21 +7,35 @@
         <div
             class="relative w-full overflow-hidden rounded-xl border border-white/10"
             style="padding-top: 56.25%;"
+            wire:ignore
         >
-            <div id="env-yt-player" class="absolute inset-0 w-full h-full"></div>
+            @if ($event->reveal_animation)
+                <div id="env-yt-player" class="absolute inset-0 w-full h-full"></div>
+            @else
+                <iframe
+                    src="{{ $event->youtube_embed_url }}"
+                    class="absolute inset-0 w-full h-full"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    loading="lazy"
+                    title="{{ __('invitation.our_song') }}"
+                ></iframe>
+            @endif
         </div>
     </div>
 </section>
 
-@if ($event->youtube_video_id)
+@if ($event->youtube_video_id && $event->reveal_animation)
     <script src="https://www.youtube.com/iframe_api"></script>
     <script>
         (function () {
             let envYtPlayer = null;
+            let playerReady = false;
             let pendingPlay = false;
+            let initStarted = false;
 
-            function playEnvYoutube() {
-                if (envYtPlayer && typeof envYtPlayer.playVideo === 'function') {
+            function playWhenReady() {
+                if (playerReady && envYtPlayer && typeof envYtPlayer.playVideo === 'function') {
                     envYtPlayer.playVideo();
                     pendingPlay = false;
                     return;
@@ -30,7 +44,13 @@
                 pendingPlay = true;
             }
 
-            window.onYouTubeIframeAPIReady = function () {
+            function initYtPlayer() {
+                if (initStarted || ! document.getElementById('env-yt-player')) {
+                    return;
+                }
+
+                initStarted = true;
+
                 envYtPlayer = new YT.Player('env-yt-player', {
                     videoId: @json($event->youtube_video_id),
                     playerVars: {
@@ -43,17 +63,38 @@
                     },
                     events: {
                         onReady: function () {
+                            playerReady = true;
                             window.envYtPlayer = envYtPlayer;
 
                             if (pendingPlay) {
-                                playEnvYoutube();
+                                playWhenReady();
                             }
                         },
                     },
                 });
-            };
+            }
 
-            document.addEventListener('invitation:revealed', playEnvYoutube);
+            function ensureApiThenInit() {
+                if (window.YT && window.YT.Player) {
+                    initYtPlayer();
+                    return;
+                }
+
+                const prevCallback = window.onYouTubeIframeAPIReady;
+
+                window.onYouTubeIframeAPIReady = function () {
+                    if (typeof prevCallback === 'function') {
+                        prevCallback();
+                    }
+
+                    initYtPlayer();
+                };
+            }
+
+            document.addEventListener('invitation:revealed', function () {
+                ensureApiThenInit();
+                playWhenReady();
+            });
         })();
     </script>
 @endif
